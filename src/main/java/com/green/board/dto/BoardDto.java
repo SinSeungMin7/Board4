08bdc6ffd4aaa52eadc0cda626dc5cca82235a59
SinSeungMin7 <min85112000@gmail.com>
2026-05-06 17:24:55 +0900

    lombok(롬복)을 이용한 BoardDto 만들기 @Getter @Setter @ToString @NoArgsConstructor  // 기본생성자 @AllArgsConstructor // 모든인자 생성자

--- src/main/java/com/green/board/dto/BoardDto.java
+++ src/main/java/com/green/board/dto/BoardDto.java
@@ -1,8 +1,18 @@
 package com.green.board.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
-@Data  // Getter/setter/toString/GashCodem equals 재정의, 기본생성자 BoardDto()
+//@Data  // Getter/setter/toString/GashCodem equals 재정의, 기본생성자 BoardDto()
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor  // 기본생성자
+@AllArgsConstructor // 모든인자 생성자
 public class BoardDto {
 	// Fields
 	private  int      idx;
