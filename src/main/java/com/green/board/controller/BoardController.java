7e92c54ab6c7f278e5af8a4c31a17ebb336b7390
SinSeungMin7 <min85112000@gmail.com>
2026-05-06 12:09:42 +0900

    Board4 수정

---
+++ src/main/java/com/green/board/controller/BoardController.java
@@ -0,0 +1,58 @@
+package com.green.board.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.green.board.dto.BoardDto;
+import com.green.board.mapper.BoardMapper;
+import com.green.menus.dto.MenuDTO;
+import com.green.menus.mapper.MenuMapper;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("/Board")
+public class BoardController {
+	
+	@Autowired
+	private  MenuMapper   menuMapper;
+	
+	@Autowired
+	private  BoardMapper  boardMapper;
+	
+	// /Board/List?menu_id=MENU01
+	@RequestMapping("/List")
+	public  ModelAndView   list( MenuDTO  menuDto  ) {
+		
+		// 메뉴전체목록 조회
+		List<MenuDTO>  menuList  =  menuMapper.getMenuList();
+		log.info("menuList:" + menuList);
+				
+		// 게시물 목록 조회
+		List<BoardDto>  boardList  =  boardMapper.getBoardList( menuDto );		
+		log.error("boardList:" + boardList);
+		
+		ModelAndView  mv  =  new  ModelAndView();
+		mv.setViewName("board/list");
+		mv.addObject("menuList",  menuList);
+		mv.addObject("boardList", boardList);
+		return  mv;
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/green/board/dto/BoardDto.java
@@ -0,0 +1,15 @@
+package com.green.board.dto;
+
+import lombok.Data;
+
+@Data
+public class BoardDto {
+	 // Fields
+	 private  int      idx;
+	 private  String   menu_id;
+	 private  String   title;
+	 private  String   writer;
+	 private  String   regdate;
+	 private  int      hit;
+	
+}
